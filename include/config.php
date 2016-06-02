8d3ef95837479fdbf300c8749dd0fa6a33152ffe
msmith624 <msmith624@outlook.com>
2016-06-02 10:36:22 -0400

    Code Challenge
    First attempt at the code challenge

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => 'password',
+    'db_name' => 'ice_cream_shop',
+    'debug'   => 'trues',
+    'x'       => 'y'
+);
